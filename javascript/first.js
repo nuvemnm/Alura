dba37c3e953a01cf81378bbbeb355db05b35a7bd
nuvemnm <matheuscarv010@gmail.com>
2024-05-20 20:43:54 -0300

    Dia 1

--- javascript/first.js
+++ javascript/first.js
@@ -1,1 +1,18 @@
 console.log("execute");
+let idade = 25;
+idade ++;
+console.log(idade);
+
+const lista = ["coisa","carro","pessoa","casa","moto"];
+
+const segundaLista = new Array("coisa","carro","pessoa","casa","moto", "força");
+
+console.log(lista[2], lista[4]);
+console.log(segundaLista[2], segundaLista[4]);
+
+lista.push("força");
+
+//lista.slice(1,1);
+
+console.log(lista.splice(2,2));
+console.log(lista);
