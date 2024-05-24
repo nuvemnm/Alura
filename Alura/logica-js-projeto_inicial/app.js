83c0ee031ca85d9b7da377fc1b8ca8e58963a253
Matheus Gregor <159480088+nuvemnm@users.noreply.github.com>
2024-05-24 11:05:17 -0300

    Add files via upload

--- Alura/logica-js-projeto_inicial/app.js
+++ Alura/logica-js-projeto_inicial/app.js
@@ -1,21 +1,36 @@
-alert('Boas vindas ao site!');
 
-let nome = "Lua";
-let idade = 25;
-let numeroDeVendas = 50;
-let saldoDisponivel = 1000;
+alert('Boas vindas ao jogo do número secreto');
 
-let mensagemDeErro = "Erro!Preencha todos os campos";
-alert(mensagemDeErro);
+// Adicione um console.log para verificar o valor de "chute" após a entrada do usuário
+let chute;
+let tentativa = 1;
+console.log('Valor do chute:', chute);
 
+let numeroSecreto = 4;
 
-let var1 = prompt("Informe seu nome:");
-nome = var1;
-let var2 = prompt("Informe sua idade:");
-idade = var2;
+// Adicione um console.log para verificar a comparação entre "chute" e "numeroSecreto"
+console.log('Resultado da comparação:', chute == numeroSecreto);
 
-if(idade >= 18){
-    alert("Pode tirar habilitação");
+while(chute != numeroSecreto){
+    chute = prompt('Escolha um número entre 1 e 10');
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if(chute < numeroSecreto){
+            alert("O numero é maior que " + chute);
+        }else{
+            alert("O numero é menor " + chute);
+        }
+        tentativa++;
+
+        // Adicione um console.log para verificar o valor de "numeroSecreto" quando o jogador erra
+        console.log('Valor do número secreto:', numeroSecreto)
+    }
+}
+console.log("Tentativas: " + tentativa);
+
+if(tentativa > 1){
+    alert("Você descobriu com " + tentativa + " tentativas");
 }else{
-    alert("Não pode tirar habilitção");
+    alert("Você descobriu com " + tentativa + " tentativa");
 }
