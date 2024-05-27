27df39f48763a41296fd7221ab14d63519c7f563
Matheus Gregor <159480088+nuvemnm@users.noreply.github.com>
2024-05-27 11:30:11 -0300

    Add files via upload

--- Alura/logica-js-projeto_inicial/app.js
+++ Alura/logica-js-projeto_inicial/app.js
@@ -2,17 +2,18 @@
 alert('Boas vindas ao jogo do número secreto');
 
 // Adicione um console.log para verificar o valor de "chute" após a entrada do usuário
+
+let numeroMax = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMax + 1);
 let chute;
 let tentativa = 1;
 console.log('Valor do chute:', chute);
-
-let numeroSecreto = 4;
 
 // Adicione um console.log para verificar a comparação entre "chute" e "numeroSecreto"
 console.log('Resultado da comparação:', chute == numeroSecreto);
 
 while(chute != numeroSecreto){
-    chute = prompt('Escolha um número entre 1 e 10');
+    chute = prompt(`Escolha um número entre 1 e ${numeroMax}`);
     if (chute == numeroSecreto) {
         break;
     } else {
@@ -29,8 +30,8 @@ while(chute != numeroSecreto){
 }
 console.log("Tentativas: " + tentativa);
 
-if(tentativa > 1){
-    alert("Você descobriu com " + tentativa + " tentativas");
-}else{
-    alert("Você descobriu com " + tentativa + " tentativa");
-}
+let palavraTentativa = tentativa > 1? "tentativas" : "tentativa";
+alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativa} ${palavraTentativa}`);
+
+
+
